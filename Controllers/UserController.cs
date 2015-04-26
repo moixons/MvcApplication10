3cbb3500bf0ba17deec03cef01aec04d573984c0
moixons <joan.calzada@gmail.com>
2015-04-26 22:47:33 +0200

    Millores mvc

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -5,11 +5,16 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
+using System.Configuration;
 
 namespace MvcApplication10.Controllers
-{
+{    
+
     public class UserController : Controller
     {
+        private static Helpers.SessionManager _session;
+        private static string CONNECTIONSTRING = ConfigurationManager.ConnectionStrings["TestingDB"].ConnectionString;
+
         public ActionResult Index()
         {
             return View();
@@ -22,11 +27,25 @@ namespace MvcApplication10.Controllers
         }
 
         [HttpPost]
-        public ActionResult logIn(Models.UserModel user)
+        public ActionResult logIn(SharedModels.Models.UserModel user)
         {
-            if (Isvalid(user.Email, user.Password))
+            Helpers.Utils.validateUser(user);
+            ConnectionsManager.Users conn = new ConnectionsManager.Users(CONNECTIONSTRING);
+            int idUser = conn.existUser(user.Email, user.Password);
+            if ( idUser > 0)
             {
-                FormsAuthentication.SetAuthCookie(user.Email, false);
+                if (_session == null)
+                {
+                    _session = new Helpers.SessionManager();
+                    _session.setIsLoggedIn(user.Email);
+                    FormsAuthentication.SetAuthCookie(user.Email, false);
+                }
+                else
+                {
+                    _session.setIsLoggedIn(user.Email);
+                    FormsAuthentication.SetAuthCookie(user.Email, false);
+                }
+                conn.AddRegister(idUser, _session.GetSessionId(), Request.UserHostAddress);
                 return RedirectToAction("List", "User");
             }
             else
@@ -42,90 +61,60 @@ namespace MvcApplication10.Controllers
         }
 
         [HttpPost]
-        public ActionResult Registration(Models.UserModel user)
+        public ActionResult Registration(SharedModels.Models.UserModel user)
         {
             if (ModelState.IsValid )
             {
-                using (var db = new TestingEntities())
+                Helpers.Utils.validateUser(user);
+                user.IP = Request.UserHostAddress;
+                ConnectionsManager.Users conn = new ConnectionsManager.Users(CONNECTIONSTRING);
+                if (conn.existUser(user.Email, user.Password) > 0)
                 {
-                    var sysUser = db.USERS.Create();
-                    sysUser.NAME = user.Name;
-                    sysUser.EMAIL = user.Email;
-                    sysUser.PASSWORD = user.Password;
-                    DateTime date = Convert.ToDateTime(user.BirthDay);
-                    sysUser.BIRTHDAY = date;
-                    sysUser.COUNTRY = user.Country;
-                    sysUser.AVATAR = null;
-
-                    db.USERS.Add(sysUser);
-                    db.SaveChanges();
-                    int lastId = db.USERS.Max(item => item.ID);
-                    InsertLogin(lastId);
-                    FormsAuthentication.SetAuthCookie(user.Email, false);
-
-                    return RedirectToAction("List", "User");                    
+                    ModelState.AddModelError("", "User exist with the same email and password");
+                    return View();
+                }
+                else
+                {
+                    conn.AddUser(user);                    
                 }
             }
             else
             {
                 ModelState.AddModelError("", "Login data is incorrect.");
+                return View();
             }
-            return View();
+            return RedirectToAction("logIn", "User");
         }
 
         public ActionResult LogOut()
         {
+            ConnectionsManager.Users conn = new ConnectionsManager.Users(CONNECTIONSTRING);
+            if (_session != null)
+            {
+                conn.updateRegister(_session.GetSessionId());
+            }
+            else
+            {
+                _session = new Helpers.SessionManager();
+                conn.updateRegister(_session.GetSessionId());
+            }
             FormsAuthentication.SignOut();
             return RedirectToAction("Index", "Home");
         }
 
-        private bool Isvalid(string Email, string password)
-        {
-            bool Isvalid = false;
-            using (var db = new TestingEntities())
-            {
-                var user = db.USERS.FirstOrDefault(u => u.EMAIL == Email  && u.PASSWORD == password);
-                if (user !=null)
-                {                    
-                    Isvalid = true;
-                    InsertLogin(user.ID);                   
-                }
-            }
-            return Isvalid;
-        }
-
-        private void InsertLogin(int idUser)
-        {
-            using (var db = new TestingEntities())
-            {
-                var register = db.REGISTER.FirstOrDefault(u => u.IDUSER == idUser);
-                if (register != null)
-                {
-                    register.DLOGIN = DateTime.Now;
-                    register.CONNECT = true;
-                    register.COUNT = (register.COUNT + 1);
-                } 
-                else 
-                {
-                    var registerNew = db.REGISTER.Create();
-                    registerNew.CONNECT = true;
-                    registerNew.DLOGIN = DateTime.Now;
-                    registerNew.IDUSER = idUser;
-                    db.REGISTER.Add(registerNew);                    
-                }
-                db.SaveChanges();               
-            }
-        }
-
         public ActionResult List()
         {
-            var users = new List<USERS>();
+            
+            ConnectionsManager.Users conn = new ConnectionsManager.Users(CONNECTIONSTRING);
+            List<SharedModels.Models.ListUsers> listUsers = conn.getListUsers();
+            return View(listUsers);
+            /*
+             * var users = new List<USERS>();
             using (TestingEntities dc = new TestingEntities())
             {
                 users = dc.USERS.ToList();
             }
-            return View(users);
-            /*
+             * 
             using (TestingEntities dc = new TestingEntities())
             {
                 var users = dc.USERS.Join(dc.REGISTER, u => u.ID, r => r.IDUSER, (u, r) => new { USERS = u, REGISTER = r }).ToList();
@@ -133,5 +122,16 @@ namespace MvcApplication10.Controllers
             }
             */
         }
+
+        public ActionResult Autocomplete(string term)
+        {
+            var items = new[] { "Apple", "Pear", "Banana", "Pineapple", "Peach" };
+
+            var filteredItems = items.Where(
+                item => item.IndexOf(term, StringComparison.InvariantCultureIgnoreCase) >= 0
+                );
+            return Json(filteredItems, JsonRequestBehavior.AllowGet);
+        }
+
     }
 }
